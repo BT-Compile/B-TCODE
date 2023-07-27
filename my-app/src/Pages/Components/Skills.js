7d9ac7e414865323a40f79f8fb5821cb21ee519a
Ben Trang <121653356+BT-Compile@users.noreply.github.com>
2023-07-27 12:23:19 -0400

    work

--- my-app/src/Pages/Components/Skills.js
+++ my-app/src/Pages/Components/Skills.js
@@ -3,25 +3,25 @@ import React from "react";
 export default function Skills() {
     return (
         <>
-            <h2 className="headalign">Proficiencies / Skills</h2>
+            <h2 className="headalign">Skills and Projects</h2>
 
             <div className="containerText3">
 
-                <div className='skillsbox'>
-                    <h3>Projects</h3>
-                    <p>&#x2022; Discord greeter bot (JavaScript)</p>
-                    <p>&#x2022; Small </p>
-                    <p>&#x2022; This website (React.js)</p>
-                </div>
-                <div className='skillsbox'>
+            <div className='skillsbox'>
                     <h3>Information Technology Skills</h3>
-                    <p>&#x2022; System Analysis</p>
+                    <p>&#x2022; Business Process Modeling and Analysis</p>
                     <p>&#x2022; Cloud Based Architecture</p>
                     <p>&#x2022; Network Analysis</p>
+                    <p>&#x2022; System Analysis</p>
                     <p>&#x2022; Database Design</p>
-                    <p>&#x2022; Business Process Modeling and Analysis</p>
                 </div>
-                
+
+                <div className='skillsbox'>
+                    <h3>Projects</h3>
+                    <p>&#x2022; Small Database with GUI and password hashing (Java)</p>
+                    <p>&#x2022; Small Discord greeter bot (JavaScript)</p>
+                    <p>&#x2022; This website! (React.js)</p>
+                </div>
             </div>
         </>
     );
